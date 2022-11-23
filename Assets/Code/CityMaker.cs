634750485e402cf5f10ebcc6c08c73f6c8c15c61
Paulo Ogando Gulias <A01751587@itesm.mx>
2022-11-23 14:56:02 -0600

    Rotacion de semafotos - Prueba

--- Assets/Code/CityMaker.cs
+++ Assets/Code/CityMaker.cs
@@ -12,10 +12,18 @@ public class CityMaker : MonoBehaviour
     [SerializeField] GameObject destiny2Prefab;
     [SerializeField] GameObject semaphorePrefab;
     [SerializeField] int tileSize;
+    public List<(int, int)> semaforosArriba;
+    public List<(int, int)> semaforosAbajo;
+    public List<(int, int)> semaforosDerecha;
+    public List<(int, int)> semaforosIzquierda;
 
     // Start is called before the first frame update
     void Start()
     {
+        semaforosArriba = new List<(int, int)> {(0, 12), (1, 12), (6, 1), (7, 1), (13, 1), (14, 1)};
+        semaforosAbajo = new List<(int, int)> {(6, 15), (7, 15), (22, 6), (23, 6), (16, 21), (17, 21)};
+        semaforosDerecha = new List<(int, int)> {(2, 11), (2, 10), (8, 17), (8, 16), (18, 22), (18, 23)};
+        semaforosIzquierda = new List<(int, int)> {(5, -1), (5, 0), (12, -1), (12, 0), (21, 8), (21, 7)};
         MakeTiles(layout.text);
     }
 
@@ -53,15 +61,47 @@ public class CityMaker : MonoBehaviour
                 position = new Vector3(x * tileSize, 0, y * tileSize);
                 tile = Instantiate(roadPrefab, position, Quaternion.identity);
                 tile.transform.parent = transform;
-                tile = Instantiate(semaphorePrefab, position, Quaternion.identity);
+                if (semaforosArriba.Contains(tiles[i])) { 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 90, 0));
+                }
+                else if (semaforosAbajo.Contains(tiles[i])) { 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, -90, 0));
+                }
+                else if (semaforosIzquierda.Contains(tiles[i])) { 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 180, 0));
+                }
+                else if (semaforosDerecha.Contains(tiles[i])) { 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 0, 0));
+                }
                 tile.transform.parent = transform;
                 x += 1;
             } else if (tiles[i] == 'S') {
                 position = new Vector3(x * tileSize, 0, y * tileSize);
-                tile = Instantiate(roadPrefab, position, Quaternion.Euler(0, 90, 0));
-                tile.transform.parent = transform;
-                tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 90, 0));
-                tile.transform.parent = transform;
+                
+                if (semaforosArriba.Contains(tiles[(x, y)])) {
+                    tile = Instantiate(roadPrefab, position, Quaternion.Euler(0, 90, 0));
+                    tile.transform.parent = transform; 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 90, 0));
+                    tile.transform.parent = transform;
+                }
+                else if (semaforosAbajo.Contains(tiles[i])) { 
+                    tile = Instantiate(roadPrefab, position, Quaternion.Euler(0, 90, 0));
+                    tile.transform.parent = transform;
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, -90, 0));
+                    tile.transform.parent = transform;
+                }
+                else if (semaforosIzquierda.Contains((x, y))) {
+                    tile = Instantiate(roadPrefab, position, Quaternion.Euler(0, 90, 0));
+                    tile.transform.parent = transform; 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 180, 0));
+                    tile.transform.parent = transform;
+                }
+                else if (semaforosDerecha.Contains(tiles[i])) {
+                    tile = Instantiate(roadPrefab, position, Quaternion.Euler(0, 90, 0));
+                    tile.transform.parent = transform; 
+                    tile = Instantiate(semaphorePrefab, position, Quaternion.Euler(0, 0, 0));
+                    tile.transform.parent = transform;
+                }
                 x += 1;
             } else if (tiles[i] == 'D') {
                 position = new Vector3(x * tileSize, 0, y * tileSize);
