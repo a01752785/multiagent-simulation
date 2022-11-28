ffa2d8f3083005839eb91b61ff5f1908beaad818
BioEduardo <67594938+BioEduardo@users.noreply.github.com>
2022-11-28 13:07:42 -0600

    5A

--- modeloMesa/agent.py
+++ modeloMesa/agent.py
@@ -141,11 +141,8 @@ class Car(Agent):
                 if pos[0][0] < self.model.width and pos[0][1] < self.model.height:
                     content = self.model.grid.get_cell_list_contents([pos[0]])
                     for cell in content:
-                        if isinstance(cell, Car):
-                            new_position = self.pos
-                            continue
                         if (isinstance(cell, Road) and ((cell.direction != self.direction and cell.direction != pos[1]) 
-                        or cell.direction == self.direction)) or isinstance(cell, Destination) and pos[0] == self.destino:
+                        or cell.direction == self.direction)) or (isinstance(cell, Destination) and pos[0] == self.destino) and isinstance(cell, Car):
                             distancia_nueva = self.calcularDistancia(self.destino, pos[0])
                             if distancia_nueva < distancia_minima:
                                 distancia_minima = distancia_nueva
